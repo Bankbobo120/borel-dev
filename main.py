f18f1d10289f28e46b743ae434e96acf68a80db2
Bankbobo120 <121422388+Bankbobo120@users.noreply.github.com>
2025-07-08 02:24:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TELEGRAM_TOKEN = "123456789:ABCdefGhIJKlmNoPQRstUVwxyZ"  # <-- Mets ici TON token Telegram
+TELEGRAM_TOKEN = "7295542974:AAGIjBZjzktAHBIz0QPlvE-aD3QYUca7yEc"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bonjour")
+    await update.message.reply_text("Bonjour 👋")
 
 async def main():
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    await app.run_polling()
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
 
-if __name__ == "__main__":
-    import asyncio
-    asyncio.get_event_loop().run_until_complete(main())
+import asyncio
+asyncio.get_event_loop().create_task(main())
+asyncio.get_event_loop().run_forever()
